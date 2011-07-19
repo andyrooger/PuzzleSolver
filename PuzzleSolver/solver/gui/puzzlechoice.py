8a5e3ff1772a0f21abf617a2ac450647f503d64f
Andy <as.gurden@gmail.com>
2011-07-19 15:46:39 +0100

    Started using state in puzzle choice rather than passing things

--- PuzzleSolver/solver/gui/puzzlechoice.py
+++ PuzzleSolver/solver/gui/puzzlechoice.py
@@ -5,19 +5,21 @@ Contains puzzle type selection controls.
 
 import tkinter
 
+import solver.state
+
 from .. utility.buttonselector import ButtonSelector
 
 class PuzzleChoice(tkinter.Frame):
     """Puzzle type selection widget."""
 
-    def __init__(self, master, plugins):
+    def __init__(self, master):
         tkinter.Frame.__init__(self, master)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
         tkinter.Label(self, text="Choose a puzzle type:").grid(row=0, sticky="new")
 
         self.selector = ButtonSelector(self, vertical=True)
-        for plugin in plugins:
+        for plugin in solver.state.puzzle.allowable:
             self.selector.add(plugin.name(), plugin)
         self.selector.grid(row=1, sticky="nsew")
 
