93cbafdf6391068953d7c09e4d386d692a3612c3
Andy <as.gurden@gmail.com>
2011-07-19 19:56:36 +0100

    Modified plugin instructions

--- PuzzleSolver/plugins/__init__.py
+++ PuzzleSolver/plugins/__init__.py
@@ -3,6 +3,6 @@ Package to house all puzzle plugins.
 
 They should be importable directly from this directory, with the main
 plugin class in any contained module being called Puzzle and inheriting from
-solver.plugins.PuzzleType.
+solver.plugin.PuzzleType.
 
 """
