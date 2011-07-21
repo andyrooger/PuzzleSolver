9cf62e51fb954b27ba55da2940adcd7be0e2bd7b
Andy <as.gurden@gmail.com>
2011-07-21 17:53:39 +0100

    Added solver to concrete plugin

--- PuzzleSolver/plugins/concrete.py
+++ PuzzleSolver/plugins/concrete.py
@@ -4,8 +4,12 @@ Various base classes for plugins.
 """
 
 import tkinter
+import multiprocessing
+import queue
+import time
 
 import solver.plugin
+import solver.state
 
 _i = 0
 
@@ -48,15 +52,17 @@ class ConcreteView(solver.plugin.PuzzleView):
         fr.grid_columnconfigure(0, weight=1)
         tkinter.Label(fr, text="Hello I am " + str(self.i) + " and my mode is " + self.mode).grid(row=0, column=0, sticky="nsew")
         tkinter.Entry(fr, textvariable=self.data).grid(row=1, column=0, sticky="sew")
+        self.status = tkinter.Frame(fr, width=50, background="GREEN")
+        self.status.grid(row=0, column=1, rowspan=2, sticky="nse")
         return fr
 
     def canSolve(self):
         """Can we solve puzzles in this view."""
-        return self.i == 3
+        return self.i > 4 and self.mode == "PLAY"
 
     def getSolver(self):
         """Get the solver for this view if one exists."""
-        return ConcreteSolver() if self.i == 3 else None
+        return ConcreteSolver(self.data, self.status) if self.canSolve() else None
 
     def getExtension(self):
         """Get either the file extension used to save the puzzles below, or None."""
@@ -85,13 +91,50 @@ class ConcreteView(solver.plugin.PuzzleView):
 class ConcreteSolver(solver.plugin.Solver):
     """Functionality for a puzzle solver, changed will be performed on the underlying view."""
 
+    def __init__(self, var, status):
+        solver.plugin.Solver.__init__(self)
+        self.var = var
+        self.status = status
+        self.queue = multiprocessing.Queue()
+        self.proc = None
+
     def start(self):
         """Start the solver."""
 
-        print("Solving...")
+        self.proc = multiprocessing.Process(target=self.solve)
+        self.proc.start()
+        self.poll()
 
     def stop(self):
         """Stop the solver and return success (in stopping)."""
 
-        print("Done.")
-        return True
+        return self.proc == None or not self.proc.is_alive()
+
+    def poll(self): # Ergh
+        if self.proc == None:
+            return
+        lastitem = None
+        while True:
+            try:
+                lastitem = self.queue.get(False)
+
+                if lastitem == False: # Done
+                    self.proc.join()
+                    self.proc = None
+                    solver.state.solving.change(None)
+                    return
+
+                self.status.config(bg=lastitem)
+            except queue.Empty:
+                break
+
+        self.status.after(10, self.poll)
+
+    def solve(self):
+        for i in range(10):
+            self.queue.put("RED")
+            time.sleep(0.5)
+            self.queue.put("ORANGE")
+            time.sleep(0.5)
+        self.queue.put("GREEN")
+        self.queue.put(False)
