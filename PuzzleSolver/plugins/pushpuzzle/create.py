1c1825f9f50ccca70726d26323562e5538de62c9
Andy <as.gurden@gmail.com>
2011-07-22 13:04:07 +0100

    Moved creation gui into a new class

--- PuzzleSolver/plugins/pushpuzzle/create.py
+++ PuzzleSolver/plugins/pushpuzzle/create.py
@@ -12,27 +12,33 @@ class CreateView(solver.plugin.PuzzleView):
 
     def __init__(self):
         solver.plugin.PuzzleView.__init__(self)
+        self.frame = None
 
     def getFrame(self, master):
-        return tkinter.Label(master, text="Create Mode!!!")
+        self.frame = CreateFrame(master)
+        return self.frame
 
     def canSolve(self): return False
     def getSolver(self): return None
+    def getExtension(self): return ".spp"
 
-    def getExtension(self):
-        return ".spp"
+    def getPuzzle(self): return self.frame.getPuzzle()
+    def changed(self): return self.frame.changed()
+    def saved(self): return self.frame.saved()
+    def clean(self): return self.frame.clean()
+    def load(self, puzzle): return self.frame.load(puzzle)
 
-    def getPuzzle(self):
-        return "The puzzle"
+class CreateFrame(tkinter.Frame):
+    """GUI for the creation view."""
 
-    def changed(self):
-        return False
+    def __init__(self, master):
+        tkinter.Frame.__init__(self, master)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        tkinter.Label(self, text="create frame").grid(stick="nsew")
 
-    def saved(self):
-        pass
-
-    def clean(self):
-        pass
-
-    def load(self, puzzle):
-        return False
+    def clean(self): pass
+    def load(self, puzzle): return True
+    def getPuzzle(self): return None
+    def saved(self): pass
+    def changed(self): return False
