cf6a050c936157d3382612825b0580ac8d71d2c5
Andy <as.gurden@gmail.com>
2011-07-24 03:01:59 +0100

    Added action to create button on dimension chooser

---
+++ PuzzleSolver/plugins/pushpuzzle/puzzle.py
@@ -0,0 +1,11 @@
+"""
+Contains the structures to store our puzzles in.
+
+"""
+
+class Puzzle:
+    """Main class for storing our puzzles."""
+
+    def __init__(self, h, w):
+        self.height = h
+        self.width = w
