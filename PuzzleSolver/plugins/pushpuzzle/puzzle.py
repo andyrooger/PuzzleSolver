2b84058f1375b99bf818837dc87fd0392c106e41
Andy <as.gurden@gmail.com>
2011-07-25 13:30:39 +0100

    Added validation to puzzle

--- PuzzleSolver/plugins/pushpuzzle/puzzle.py
+++ PuzzleSolver/plugins/pushpuzzle/puzzle.py
@@ -13,10 +13,31 @@ class Puzzle:
         self.targets = set()
         self.states = [initial] if initial != None else [PuzzleState()]
 
-    def __getitem__(self, index):
-        return self.states[index]
+    def inArea(self, x, y):
+        """Are the given coordinates within our range?"""
 
-    # THINK ABOUT HOW TO EDIT THE STATES PROPERLY
+        if x < 0 or x >= self.width:
+            return False
+        if y < 0 or y >= self.height:
+            return False
+        return True
+
+    def valid(self, children=True):
+        """Is this a valid setup?"""
+
+        if not all(self.inArea(x, y) for x, y in self.walls.union(self.targets)):
+            return False
+
+        if children and not all(s.valid(self) for s in self.states):
+            return False
+
+        if self.walls.intersection(self.targets):
+            return False
+
+        if children and not all(len(self.states[0].boxes) == len(s.boxes) for s in self.states):
+            return False
+
+        return True
 
 class PuzzleState:
     """Information on parts of the puzzle that can change during the play."""
@@ -25,11 +46,17 @@ class PuzzleState:
         self.player = None
         self.boxes = set()
 
-# - dimensions (stationary)
-# - wall locations (stationary)
-# - target locations (stationary)
-# - box locations (mobile)
-# - player location (mobile)
-# * nothing can be in same place as a wall
-# * player cannot be in same place as box
-# * coords have to be inside dimensions
+    def valid(self, parent):
+        if self.player == None or not parent.inArea(*self.player):
+            return False
+
+        if not all(parent.inArea(x, y) for (x, y) in self.boxes):
+            return False
+
+        if self.player in parent.walls.union(self.boxes):
+            return False
+
+        if self.boxes.intersection(parent.walls):
+            return False
+
+        return True
