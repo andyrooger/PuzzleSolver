e013c300eefab4525bc5217b9bb92a8e720d28f0
Andy <as.gurden@gmail.com>
2011-07-25 14:18:05 +0100

    Added goal test for puzzle states

--- PuzzleSolver/plugins/pushpuzzle/puzzle.py
+++ PuzzleSolver/plugins/pushpuzzle/puzzle.py
@@ -6,12 +6,12 @@ Contains the structures to store our puzzles in.
 class Puzzle:
     """Main class for storing our puzzles."""
 
-    def __init__(self, h, w, initial = None):
+    def __init__(self, h, w):
         self.height = h
         self.width = w
         self.walls = set()
         self.targets = set()
-        self.states = [initial] if initial != None else [PuzzleState()]
+        self.states = [PuzzleState(self)]
 
     def inArea(self, x, y):
         """Are the given coordinates within our range?"""
@@ -28,7 +28,7 @@ class Puzzle:
         if not all(self.inArea(x, y) for x, y in self.walls.union(self.targets)):
             return False
 
-        if children and not all(s.valid(self) for s in self.states):
+        if children and not all(s.valid() for s in self.states):
             return False
 
         if self.walls.intersection(self.targets):
@@ -42,44 +42,51 @@ class Puzzle:
 class PuzzleState:
     """Information on parts of the puzzle that can change during the play."""
 
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
         self.player = None
         self.boxes = set()
+        self.recordAccessibility()
 
-    def valid(self, parent):
-        if self.player == None or not parent.inArea(*self.player):
+    def valid(self):
+        if self.player == None or not self.parent.inArea(*self.player):
             return False
 
-        if not all(parent.inArea(x, y) for (x, y) in self.boxes):
+        if not all(self.parent.inArea(x, y) for (x, y) in self.boxes):
             return False
 
-        if self.player in parent.walls.union(self.boxes):
+        if self.player in self.parent.walls.union(self.boxes):
             return False
 
-        if self.boxes.intersection(parent.walls):
+        if self.boxes.intersection(self.parent.walls):
             return False
 
         return True
 
-    def recordAccessibility(self, parent):
+    def recordAccessibility(self):
         """Create and record a set describing accessible coordinates from this state."""
 
         self.accessible = set()
         if self.player != None:
-            self._expandAccessible(parent, self.player)
+            self._expandAccessible(self.player)
 
-    def _expandAccessible(self, parent, pos):
+    def _expandAccessible(self, pos):
         """Open up accessibility from the position pos."""
 
-        if (parent.inArea(*pos) and
+        if (self.parent.inArea(*pos) and
             pos not in self.accessible and
-            pos not in parent.walls and
+            pos not in self.parent.walls and
             pos not in self.boxes):
 
             self.accessible.add(pos)
 
             x, y = pos
-            self._expandAccessible(parent, (x-1, y))
-            self._expandAccessible(parent, (x+1, y))
-            self._expandAccessible(parent, (x, y-1))
-            self._expandAccessible(parent, (x, y+1))
+            self._expandAccessible(self.parent, (x-1, y))
+            self._expandAccessible(self.parent, (x+1, y))
+            self._expandAccessible(self.parent, (x, y-1))
+            self._expandAccessible(self.parent, (x, y+1))
+
+    def goal(self):
+        """Have we achieved our goal?"""
+
+        return self.parent.targets.difference(self.boxes)
