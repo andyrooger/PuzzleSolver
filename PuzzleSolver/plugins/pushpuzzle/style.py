6ade95f1049ffcc842e8ee00e0034440ac4ba026
Andy <as.gurden@gmail.com>
2011-07-26 13:18:32 +0100

    Added style module to avoid reloading icons

---
+++ PuzzleSolver/plugins/pushpuzzle/style.py
@@ -0,0 +1,37 @@
+"""
+Helps with a consistent style and avoids loading images more than once.
+
+"""
+
+import tkinter
+import os.path
+
+icons = {}
+
+def loadIcon(name):
+    """Load and record all available icons."""
+
+    global icons
+
+    if name in icons:
+        return icons[name]
+
+    file = os.path.dirname(__file__)
+    file = os.path.join(file, "resources", "images", name.lower() + ".gif")
+    icons[name] = tkinter.PhotoImage(file=file)
+    return icons[name]
+
+def tileStyle(content, target):
+    """Get the style for a certain tile."""
+
+    bg = "blue" if content == "WALL" else "white"
+    abg = "darkblue" if content == "WALL" else "gray"
+    icon = (loadIcon(content)
+            if content in ["PLAYER", "BOX"]
+            else loadIcon("BLANK"))
+    hbg = "red" if target else "lightgray"
+
+    return {"bg": bg, "image": icon,
+            "highlightbackground": hbg,
+            "highlightthickness": 3,
+            "activebackground": abg}
