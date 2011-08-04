cdcbe704e5ec162aeb0a2f6186ff5f6aca00a73d
Andy <as.gurden@gmail.com>
2011-08-04 02:54:25 +0100

    Made more items private in ScrollableWindow

--- PuzzleSolver/solver/utility/scrollable_window.py
+++ PuzzleSolver/solver/utility/scrollable_window.py
@@ -13,30 +13,30 @@ class ScrollableWindow(tkinter.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         
-        self.vscrollbar = _AutoScrollbar(self, orient=tkinter.VERTICAL)
-        self.vscrollbar.grid(row=0, column=1, sticky="wns")
-        self.hscrollbar = _AutoScrollbar(self, orient=tkinter.HORIZONTAL)
-        self.hscrollbar.grid(row=1, column=0, sticky="new")
+        self._vscrollbar = _AutoScrollbar(self, orient=tkinter.VERTICAL)
+        self._vscrollbar.grid(row=0, column=1, sticky="wns")
+        self._hscrollbar = _AutoScrollbar(self, orient=tkinter.HORIZONTAL)
+        self._hscrollbar.grid(row=1, column=0, sticky="new")
 
-        self.scroller = tkinter.Canvas(self,
-                                       yscrollcommand=self.vscrollbar.set,
-                                       xscrollcommand=self.hscrollbar.set)
-        self.vscrollbar.config(command=self.scroller.yview)
-        self.hscrollbar.config(command=self.scroller.xview)
+        self._scroller = tkinter.Canvas(self,
+                                       yscrollcommand=self._vscrollbar.set,
+                                       xscrollcommand=self._hscrollbar.set)
+        self._vscrollbar.config(command=self._scroller.yview)
+        self._hscrollbar.config(command=self._scroller.xview)
 
-        self.window = tkinter.Frame(self.scroller)
+        self.window = tkinter.Frame(self._scroller)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.window.grid(sticky="nsew")
         self.window.bind('<Configure>', self._frame_resized)
         
-        self.scroller.create_window(0, 0, anchor="nw", window=self.window)
-        self.scroller.grid(row=0, column=0)
+        self._scroller.create_window(0, 0, anchor="nw", window=self.window)
+        self._scroller.grid(row=0, column=0)
         
     def _frame_resized(self, event=None):
         self.window.update_idletasks()
-        _1, _2, w, h = self.scroller.bbox("all")
-        self.scroller.config(scrollregion=self.scroller.bbox("all"), width=w, height=h)
+        _1, _2, w, h = self._scroller.bbox("all")
+        self._scroller.config(scrollregion=self._scroller.bbox("all"), width=w, height=h)
         
     def centre_on(self, widget, x=None, y=None):
         """Centre the window on the given position within the widget. (MUST be an ancestor of window)"""
@@ -55,17 +55,17 @@ class ScrollableWindow(tkinter.Frame):
         # now widget is self.window
         # we have a wanted centre in self.window coordinates
         # find where we want top left
-        x -= self.scroller.winfo_width() // 2
-        y -= self.scroller.winfo_height() // 2
+        x -= self._scroller.winfo_width() // 2
+        y -= self._scroller.winfo_height() // 2
         # normalise
         x /= widget.winfo_width()
         y /= widget.winfo_height()
         # clamp
         x = max(0, min(x, 1))
-        y= max(0, min(y, 1))
+        y = max(0, min(y, 1))
         # scroll
-        self.scroller.xview_moveto(x)
-        self.scroller.yview_moveto(y)
+        self._scroller.xview_moveto(x)
+        self._scroller.yview_moveto(y)
 
 class _AutoScrollbar(tkinter.Scrollbar):
     """Copied from http://effbot.org/zone/tkinter-autoscrollbar.htm"""
