c4477caa7938a32da8c831012db32f7e761653a0
Andy <as.gurden@gmail.com>
2011-08-04 03:20:18 +0100

    Corrected style of concrete plugin

--- PuzzleSolver/plugins/concrete.py
+++ PuzzleSolver/plugins/concrete.py
@@ -19,50 +19,50 @@ class Puzzle(solver.plugin.PuzzleType):
     def __init__(self):
         global _i
         solver.plugin.PuzzleType.__init__(self)
-        self.i = _i
+        self._i = _i
         _i += 1
 
     def name(self):
         """Get the name of the puzzle type."""
-        return "Concrete " + str(self.i)
+        return "Concrete " + str(self._i)
 
     def get(self, mode):
         """Get the a new puzzle pane for the given mode."""
-        return ConcreteView(mode, self.i)
+        return ConcreteView(mode, self._i)
 
 class ConcreteView(solver.plugin.PuzzleView):
     """Functionality for a single mode."""
 
     def __init__(self, mode, i):
         solver.plugin.PuzzleView.__init__(self)
-        self.mode = mode
-        self.i = i
-        self.data = tkinter.StringVar()
-        self.changeValue("", False)
+        self._mode = mode
+        self._i = i
+        self._data = tkinter.StringVar()
+        self._change_value("", False)
 
-    def changeValue(self, text, needssaving=True):
-        self.data.set(text)
+    def _change_value(self, text, needssaving=True):
+        self._data.set(text)
         if not needssaving:
-            self.oldval = text
+            self._oldval = text
 
     def get_frame(self, master):
         """Get the GUI frame."""
         fr = tkinter.Frame(master)
         fr.grid_rowconfigure(0, weight=1)
         fr.grid_columnconfigure(0, weight=1)
-        tkinter.Label(fr, text="Hello I am " + str(self.i) + " and my mode is " + self.mode).grid(row=0, column=0, sticky="nsew")
-        tkinter.Entry(fr, textvariable=self.data).grid(row=1, column=0, sticky="sew")
-        self.status = tkinter.Frame(fr, width=50, background="GREEN")
-        self.status.grid(row=0, column=1, rowspan=2, sticky="nse")
+        tkinter.Label(fr, text="Hello I am " + str(self._i) + " and my mode is " + self._mode).grid(row=0, column=0, sticky="nsew")
+        tkinter.Entry(fr, textvariable=self._data).grid(row=1, column=0, sticky="sew")
+        self._status = tkinter.Frame(fr, width=50, background="GREEN")
+        self._status.grid(row=0, column=1, rowspan=2, sticky="nse")
         return fr
 
     def can_solve(self):
         """Can we solve puzzles in this view."""
-        return self.i > 4 and self.mode == "PLAY"
+        return self._i > 4 and self._mode == "PLAY"
 
     def get_solver(self):
         """Get the solver for this view if one exists."""
-        return ConcreteSolver(self.data, self.status) if self.can_solve() else None
+        return ConcreteSolver(self._data, self._status) if self.can_solve() else None
 
     def get_extension(self):
         """Get either the file extension used to save the puzzles below, or None."""
@@ -70,23 +70,23 @@ class ConcreteView(solver.plugin.PuzzleView):
 
     def get_puzzle(self):
         """Get either the puzzle object if it can be saved, or None."""
-        return self.data.get()
+        return self._data.get()
 
     def changed(self):
         """Was the puzzle changed since the last call to save."""
-        return self.data.get() != self.oldval
+        return self._data.get() != self._oldval
 
     def saved(self):
         """Reset changed."""
-        self.oldval = self.data.get()
+        self._oldval = self._data.get()
 
     def clean(self):
         """Clean the view, removing all user changes."""
-        self.changeValue("")
+        self._change_value("")
 
     def load(self, puzzle):
         """Load the given puzzle if possible and return if successful."""
-        self.changeValue(str(puzzle), False)
+        self._change_value(str(puzzle), False)
         return True
 
 class ConcreteSolver(solver.plugin.Solver):
@@ -94,48 +94,48 @@ class ConcreteSolver(solver.plugin.Solver):
 
     def __init__(self, var, status):
         solver.plugin.Solver.__init__(self)
-        self.var = var
-        self.status = status
-        self.queue = multiprocessing.Queue()
-        self.proc = None
+        self._var = var
+        self._status = status
+        self._queue = multiprocessing.Queue()
+        self._proc = None
 
     def start(self):
         """Start the solver."""
 
-        self.proc = multiprocessing.Process(target=self.solve)
-        self.proc.start()
-        self.poll()
+        self._proc = multiprocessing.Process(target=self._solve)
+        self._proc.start()
+        self._poll()
 
     def stop(self):
         """Stop the solver and return success (in stopping)."""
 
-        return self.proc == None or not self.proc.is_alive()
+        return self._proc == None or not self._proc.is_alive()
 
-    def poll(self): # Ergh
-        if self.proc == None:
+    def _poll(self): # Ergh
+        if self._proc == None:
             return
         lastitem = None
         while True:
             try:
-                lastitem = self.queue.get(False)
+                lastitem = self._queue.get(False)
 
                 if lastitem == False: # Done
-                    self.proc.join()
-                    self.proc = None
+                    self._proc.join()
+                    self._proc = None
                     solver.state.solving.change(None)
                     return
 
-                self.status.config(bg=lastitem)
+                self._status.config(bg=lastitem)
             except queue.Empty:
                 break
 
-        self.status.after(10, self.poll)
+        self._status.after(10, self._poll)
 
-    def solve(self):
+    def _solve(self):
         for _ in range(10):
-            self.queue.put("RED")
+            self._queue.put("RED")
             time.sleep(0.5)
-            self.queue.put("ORANGE")
+            self._queue.put("ORANGE")
             time.sleep(0.5)
-        self.queue.put("GREEN")
-        self.queue.put(False)
+        self._queue.put("GREEN")
+        self._queue.put(False)
