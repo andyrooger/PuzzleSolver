d3c81f06f344a5aa53816c468612a368ca4175fb
Andy <as.gurden@gmail.com>
2011-08-06 02:31:54 +0100

    Started connecting solver dialog to the algorithm

---
+++ PuzzleSolver/plugins/pushpuzzle/pushastar.py
@@ -0,0 +1,36 @@
+"""
+Uses AStar to asynchronously solve a push puzzle.
+
+"""
+
+class PushAStar:
+    """Asynchronously solves a puzzle."""
+    
+    def __init__(self, puzzle):
+        self._puzzle = puzzle
+
+    def begin(self):
+        """Start the solving process."""
+        
+    def cancel(self):
+        """Try to stop the solving process and return whether successful."""
+        
+        return True
+        
+    def solving(self):
+        """Check if the solver is currently running."""
+        
+        return False
+        
+    def status(self):
+        """Returns a dictionary containing the latest information."""
+        
+        return {}
+        
+    def result(self):
+        """If the solver has completed, return directions/None. Raises IncompleteError otherwise."""
+        
+        return []
+        
+class IncompleteError(Exception):
+    """Thrown when a result is requested and the operation is not yet complete."""
