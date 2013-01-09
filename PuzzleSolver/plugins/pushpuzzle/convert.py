58a593b44917c435aa52fac51d9af32bddd530cc
Andy Gurden <as.gurden@gmail.com>
2013-01-09 22:44:37 +0000

    GPLed new code

--- PuzzleSolver/plugins/pushpuzzle/convert.py
+++ PuzzleSolver/plugins/pushpuzzle/convert.py
@@ -4,6 +4,24 @@ and our own type.
 
 """
 
+# PuzzleSolver
+# Copyright (C) 2010  Andy Gurden
+#
+#     This file is part of PuzzleSolver.
+#
+#     PuzzleSolver is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     PuzzleSolver is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with PuzzleSolver.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 from tkinter import filedialog
